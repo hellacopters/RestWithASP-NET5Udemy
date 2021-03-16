60872e95ed9ee2065b67b7304a99d19c3adfbc83
Leonardo Domingos <leo.domingoz@gmil.com>
2021-03-16 10:17:58 -0300

    Retirando using desnecessários

--- 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -2,10 +2,6 @@
 using Microsoft.Extensions.Logging;
 using RestWithASPNETUdemy.Model;
 using RestWithASPNETUdemy.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace RestWithASPNETUdemy.Controllers
 {
--- 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementation/PersonServiceImplementation.cs
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementation/PersonServiceImplementation.cs
@@ -3,7 +3,6 @@ using RestWithASPNETUdemy.Model.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 
 namespace RestWithASPNETUdemy.Services.Implementation
 {
